d2ed30820c9bd066581a47f3f4c79837deb452b4
Dainius <pastas4@gmail.com>
2017-07-13 16:45:07 +0200

    Revamp of calc-bbox, addition of a local repack script
    calc-bbox now goes point by point, thus retaining the original CSV layout.
    Should be about ready to leave staging.

--- src/staging/calc-bbox.r
+++ src/staging/calc-bbox.r
@@ -20,54 +20,54 @@
 
 library(raster)
 library(rgeos)
+library(optparse)
 
 # Input: Path to CSV file that maps campaigns to projections; path to reference point CSV file; path to output CSV file.
 # Output: An enhanced CSV file with xmin, xmax, ymin, ymax columns (in respective projections).
 
-AddExtent = function(projection_csv, data_csv, output_path)
+parser = OptionParser()
+parser = add_option(parser, c("-p", "--projection-file"), type="character", default="../data/reference/Projections.csv",
+    help="CSV file that maps projections to campaigns in reference data. (Default: %default)", metavar="path")
+parser = add_option(parser, c("-d", "--data-file"), type="character", metavar="path",
+    default="../data/reference/LoggedTrees.csv",
+    help="CSV file that lists logged trees in campaigns. (Default: %default)")
+parser = add_option(parser, c("-o", "--output-file"), type="character", metavar="path",
+    default="../data/reference/LoggedTreeBoundaries.csv", help="Output file path. (Default: %default)")
+args = parse_args(parser)
+    
+AddExtent = function(projection_csv=args[["projection-file"]], data_csv=args[["data-file"]], output_path=args[["output-file"]])
 {
     # Get the data into R format
     # Note that different scenes have different projections!
     # This means we need a file to map projections to campaigns, or else reproject all rasters to a single projection.
-    projection_csv = "../data/reference/Projections.csv"
     ProjectionMap = read.csv(projection_csv, stringsAsFactors=FALSE)
-    data_csv = "../data/reference/LoggedTrees.csv"
     DataPoints = read.csv(data_csv, stringsAsFactors=FALSE)
     DataPoints[["Campaign"]] = as.factor(DataPoints[["Campaign"]])
     coordinates(DataPoints) = ~Longitude+Latitude
     crs(DataPoints) = CRS("+proj=longlat +datum=WGS84 +no_defs")
     
-    OutputCSV = data.frame()
-    for (i in 1:length(levels(DataPoints@data$Campaign)))
+    OutputCSV = read.csv(data_csv, stringsAsFactors=FALSE)
+    CoordDF = data.frame()
+    for (i in 1:length(DataPoints))
     {
-        Campaign = levels(DataPoints@data$Campaign)[i]
-        # Select points of the current campaign
-        CampaignPoints = DataPoints[DataPoints$Campaign==Campaign,]
+        Campaign = DataPoints@data$Campaign[i]
+        Point = DataPoints[i,]
         # Transform to the right projection
-        DataUTM = spTransform(CampaignPoints, crs(ProjectionMap[ProjectionMap$Campaign==Campaign, "proj4"]))
+        DataUTM = spTransform(Point, crs(ProjectionMap[ProjectionMap$Campaign==Campaign, "proj4"]))
         
         # Buffer
         BufferedPoints = gBuffer(DataUTM, byid=TRUE, width=75, quadsegs=1) # Rhombus; fine, since we need the BB only
         # Could also use byid=FALSE and disaggregate() to dissolve them
-        xmin=c(); xmax=c(); ymin=c(); ymax=c()
-        for (n in 1:length(BufferedPoints))
-        {
-            # Extract extent values for writing to CSV
-            xmin = c(xmin, extent(BufferedPoints[n,])@xmin)
-            xmax = c(xmax, extent(BufferedPoints[n,])@xmax)
-            ymin = c(ymin, extent(BufferedPoints[n,])@ymin)
-            ymax = c(ymax, extent(BufferedPoints[n,])@ymax)
-        }
-        BufferedPoints@data$xmin = xmin
-        BufferedPoints@data$xmax = xmax
-        BufferedPoints@data$ymin = ymin
-        BufferedPoints@data$ymax = ymax
-        if (length(OutputCSV) == 0)
-            OutputCSV = BufferedPoints@data
+        # Extract extent values for writing to CSV
+        Coords = data.frame(xmin = extent(BufferedPoints)@xmin, xmax = extent(BufferedPoints)@xmax,
+            ymin = extent(BufferedPoints)@ymin, ymax = extent(BufferedPoints)@ymax)
+        if (length(CoordDF) == 0)
+            CoordDF = Coords
         else
-            OutputCSV = rbind(OutputCSV, BufferedPoints@data)
+            CoordDF = rbind(CoordDF, Coords)
     }
-    FinalCSV = read.csv(data_csv)
-    FinalCSV = cbind(FinalCSV, xmax=OutputCSV[["xmax"]]) # These need to be ordered first
+    OutputCSV = cbind(OutputCSV, CoordDF)
     write.csv(OutputCSV, output_path)
 }
+
+AddExtent()
